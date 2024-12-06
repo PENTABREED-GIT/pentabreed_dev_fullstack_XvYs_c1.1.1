89845727015f6d7dafd37c86669f6fe984d8f493
sjkim <smathj@naver.com>
2024-12-06 16:40:00 +0900

    에디터 vue로 만들기

--- src/main/resources/static/js/board/boardForm.js
+++ src/main/resources/static/js/board/boardForm.js
@@ -6,35 +6,6 @@ window.onload = async () => {
 
 async function init() {
 
-
-    // common.js 의 fnSummerNoteInit 호출 ...
-    fnSummerNoteInit();
 }
 
 
-async function fnBoardSave() {
-
-    const title = fnGetValueById('title')
-    const content = $('#summernote').summernote('code');
-
-    console.log({title, content})
-
-    const param = {
-        title, content,
-    };
-
-    const {status, data, metaData} = await fnAsyncJsonPost('/api/board/save', param)
-    console.log({status, data, metaData});
-
-
-    if(!status) {
-        alert('게시글 등록 에러')
-        return;
-    }
-
-    alert('게시글 등록 완료');
-    fnPageMove('/board/list');
-
-
-
-}
---
+++ src/main/resources/static/js/components/pagingComponent.js
@@ -0,0 +1,63 @@
+//! 뷰를 위한 페이징 컴포넌트
+const Pagination = {
+    props: {
+        clickPage: { type: Number, required: true },
+        totalCount: { type: Number, required: true },
+    },
+    emits: ['call-back'],
+    setup(props, {emit}) {
+        console.log('\n\n---------- 컴포넌트 setup ----------')
+        console.log('전달 받은 props')
+        console.log({...props})
+
+
+        const pageShowButtonCount = computed(() => 10);
+        const lastPageButtonNum = computed(() => Math.ceil(props.totalCount / limit));
+        const clickPage = computed(() => props.clickPage);
+        const totalCount = computed(() => props.totalCount);
+
+
+        const doEmit = (n) => {
+            console.log(`doEmit 호출  param = ${n}`)
+            emit('call-back', n)
+        }
+
+        return {pageShowButtonCount, lastPageButtonNum, doEmit, clickPage, totalCount}
+    },
+    template: `
+        <nav aria-label="Page navigation example">
+            <ul class="pagination" id="paging">
+        
+                <!--이전 버튼-->
+                <li class="page-item" v-if="clickPage > 1">
+                  <a class="page-link" href="#" aria-label="Previous" @click.prevent="doEmit(clickPage-1)">
+                    <span aria-hidden="true">&laquo;</span>
+                  </a>
+                </li>
+        
+                <!--페이지 버튼들-->
+                <template v-for="n in pageShowButtonCount">
+                    <template v-if="n <= lastPageButtonNum">
+                        <template v-if="n == clickPage">
+                            <li class="page-item active" v-if="n <= lastPageButtonNum">
+                                <a class="page-link" href="#" @click.prevent="doEmit(n)">{{ n }}</a>
+                            </li>
+                        </template>
+                        <template v-else>
+                            <li class="page-item" v-if="n <= lastPageButtonNum">
+                                <a class="page-link" href="#" @click.prevent="doEmit(n)">{{ n }}</a>
+                            </li>
+                        </template>
+                    </template>
+                </template>
+        
+                <!--다음 버튼-->
+                <li class="page-item" v-if="clickPage < lastPageButtonNum">
+                  <a class="page-link" href="#" aria-label="Next" @click.prevent="doEmit(clickPage+1)">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>
+            </ul>
+        </nav>
+    `
+};
